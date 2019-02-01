536f2b656cb2f22acd724417289dcda188f3885a
paulonova <paulonova@pigment.se>
2019-02-01 10:35:36 +0100

    create a privacy policy page and Cookie policy

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -15,11 +15,17 @@
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#">
-        <i class="fa fa-home" aria-hidden="true"></i> Back to About Us
-      </a> <span class="metabox__main">Our History</span></p>
-    </div>
+    <!-- show the ID of the parent of this page get_the_ID() 
+    IF ZERO (false) means don´t have parent else this page has a parent -->
+    <?php $theParent = wp_get_post_parent_id(get_the_ID());?>
+    <?php if($theParent):?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent);?>">
+          <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent);?>
+        </a> <span class="metabox__main"><?php the_title();?></span></p>
+      </div>
+    <?php endif;?>
+    
     
     <!--  
     <div class="page-links">
